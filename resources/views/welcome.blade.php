ad33e042bfb0d9a07946d71823ef65cff1192891
Ziya <ziyamm08@gmail.com>
2024-05-30 17:05:47 +0400

    Update readme.md

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -33,12 +33,12 @@
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
-                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">Home</a>
+                        <a href="{{ url('/home') }}" class="text-gray-700 underline" style="border: 1px solid #1a202c;padding: 5px;border-radius: 5px; color:black;text-decoration:none;">Home</a>
                     @else
-                        <a href="{{ route('login') }}" class="text-gray-900 dark:text-white " style="border: 1px solid #1a202c;padding: 5px;border-radius: 5px; color:black;">Login</a>
+                        <a href="{{ route('login') }}" class="text-gray-900 dark:text-white underline" style="border: 1px solid #1a202c;padding: 5px;border-radius: 5px; color:black;text-decoration:none;">Login</a>
 
                         {{-- @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
+                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline" style="border: 1px solid #1a202c;padding: 5px;border-radius: 5px; color:black;text-decoration:none;">Register</a>
                         @endif --}}
                     @endauth
                 </div>
